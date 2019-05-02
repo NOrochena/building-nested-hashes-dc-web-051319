ae84aafd52ca8e2ec83e7cd5e4515d7949efcc1c
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-02 14:40:05 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,7 @@ def fourth_challenge
         },
         {:name => "Mercutio",
          :age => "18",
-         :attitude => "hot-heated"
+         :attitude => "hot-headed"
         }
       ]
    }, 
